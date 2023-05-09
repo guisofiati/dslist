c9faa977b4e2efa23c5848a051fb85bc7daabdfd
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-05-09 10:04:01 -0300

    project created

---
+++ src/test/java/github/guisofiati/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package github.guisofiati.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
