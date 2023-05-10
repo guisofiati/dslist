9d7658b1e87e90dd0eeac4bfe7830803dd0e475a
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-05-10 10:17:12 -0300

    custom sql query and projections

--- src/main/java/github/guisofiati/dslist/services/GameService.java
+++ src/main/java/github/guisofiati/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package github.guisofiati.dslist.services;
 import github.guisofiati.dslist.dto.GameDTO;
 import github.guisofiati.dslist.dto.GameMinDTO;
 import github.guisofiati.dslist.entities.Game;
+import github.guisofiati.dslist.projections.GameMinProjection;
 import github.guisofiati.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,11 @@ public class GameService {
     public GameDTO findById(Long id) {
         Game gameEntity = gameRepository.findById(id).get();
         return new GameDTO(gameEntity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> gamesByListProjection = gameRepository.searchByList(listId);
+        return gamesByListProjection.stream().map(GameMinDTO::new).toList();
     }
 }
