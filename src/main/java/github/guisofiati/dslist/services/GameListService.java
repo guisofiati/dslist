f331b52361ae81b012aac0f58456140a32fbb5b7
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-05-12 10:01:15 -0300

    move game on list

--- src/main/java/github/guisofiati/dslist/services/GameListService.java
+++ src/main/java/github/guisofiati/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package github.guisofiati.dslist.services;
 
 import github.guisofiati.dslist.dto.GameListDTO;
 import github.guisofiati.dslist.entities.GameList;
+import github.guisofiati.dslist.projections.GameMinProjection;
 import github.guisofiati.dslist.repositories.GameListRepository;
+import github.guisofiati.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +17,27 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> gameListsEntity = gameListRepository.findAll();
         return gameListsEntity.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+//      int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        for (int i = min; i < max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
